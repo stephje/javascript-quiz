3f025be56ae0dfc9e2ea9a225ff4a2ee85d27287
stephje <s.jenkins3018@gmail.com>
2021-03-30 08:02:17 +0000

    Add css styles
    Add jaascript to hide some elements

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
+var cards = [
+    "start-card",
+    "question-card",
+    "result",
+    "score-card",
+    "highscore-card"
+]
 
-document.getElementById("start-screen").hidden = true;
-document.getElementById("question-screen").hidden = false;
-document.getElementById("score-screen").hidden = true;
+document.getElementById("start-card").hidden = false;
+document.getElementById("question-card").hidden = true;
+document.getElementById("result").hidden = true;
+document.getElementById("score-card").hidden = true;
+document.getElementById("highscore-card").hidden = true;
+
+document.getElementById("start-button").addEventListener("click", );
